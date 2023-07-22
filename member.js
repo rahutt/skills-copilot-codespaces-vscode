28bb6a06c854cfc746b8b4939262d70e6f3ae12a
rahutt <rob_hutt@msn.com>
2023-07-22 07:43:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    // Path: member.js
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "CSS", "HTML"]
+    };
+    // Path: member.js
+    return member.skills;
+}
